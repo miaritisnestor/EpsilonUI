26012685bf4f2b5cb9386075b5b2ec937fd75db7
miaritisnestor <nestoras.miaritis@vipagroup.com>
2023-02-25 08:42:14 +0200

    crud part completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using EpsilonUI;
+using EpsilonUI.StateContainers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7190/") }); //builder.HostEnvironment.BaseAddress
+builder.Services.AddSingleton<CustomerStateContainer>();
 
 await builder.Build().RunAsync();
---
+++ StateContainers/CustomerStateContainer.cs
@@ -0,0 +1,23 @@
+﻿using EpsilonDbContext.Models;
+
+namespace EpsilonUI.StateContainers
+{
+    public class CustomerStateContainer
+    {
+        private Customer? customer;
+
+        public Customer Property
+        {
+            get => customer ?? new Customer();
+            set
+            {
+                customer = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
